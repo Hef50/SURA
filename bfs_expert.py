e28d648b699b5139f7474c57fd2f64c662a7f694
Haresh M <hefgames50@gmail.com>
2026-06-23 04:12:37 -0400

    bfs and envr setup

---
+++ bfs_expert.py
@@ -0,0 +1,55 @@
+﻿from collections import deque
+from environment import Action
+
+def bfs(maze, start, goal):
+    start = tuple(start)
+    goal = tuple(goal)
+
+    queue = deque([start])
+    visited = {start}
+    parent = {}
+
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    while len(queue) > 0:
+        current = queue.popleft()
+        if current == goal:
+            path = []
+            while current in parent:
+                path.append(current)
+                current = parent[current]
+            path.append(start)
+            path.reverse()
+            return path
+        
+        for dr, dc in directions:
+            nr, nc = current[0] + dr, current[1] + dc
+            neighbor = (nr, nc)
+            if 0 <= nr < maze.shape[0] and 0 <= nc < maze.shape[1]:
+                if maze[nr, nc] == 0 and neighbor not in visited:
+                    visited.add(neighbor)
+                    parent[neighbor] = current
+                    queue.append(neighbor)
+    return None
+
+def generate_expert_actions(path):
+    if path is None or len(path) < 2:
+        return [Action.STOP]
+    
+    actions = []
+
+    delta_to_action = {
+        (-1, 0): Action.UP,
+        (1, 0): Action.DOWN,
+        (0, -1): Action.LEFT,
+        (0, 1): Action.RIGHT
+    }
+
+    for i in range(len(path) - 1):
+        current_cell = path[i]
+        next_cell = path[i + 1]
+        delta = (next_cell[0] - current_cell[0], next_cell[1] - current_cell[1])
+        actions.append(delta_to_action[delta])
+    
+    actions.append(Action.STOP)
+    return actions
---
+++ environment.py
@@ -0,0 +1,62 @@
+﻿import numpy as np
+from maze_generation import generate_maze, place_start_goal
+
+from enum import IntEnum
+
+class Action(IntEnum):
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
+    STOP = 4
+
+class MazeEnv:
+    def __init__(self, D=5):
+        self.D = D
+        self.max_steps = 20
+        self.reset()
+    
+    def reset(self):
+        self.maze = generate_maze(self.D)
+        self.start_pos, self.goal_pos = place_start_goal(self.maze)
+
+        self.agent_pos = self.start_pos
+        self.steps = 0
+
+        return (self.maze, self.agent_pos, self.goal_pos)
+
+    def step(self, action):
+        action = Action(action)
+
+        self.steps += 1
+        reward = 0
+        done = False
+        
+        moves = {
+            Action.UP: (-1, 0),
+            Action.DOWN: (1, 0),
+            Action.LEFT: (0, -1),
+            Action.RIGHT: (0, 1)
+        }
+
+        if action == Action.STOP:
+            if tuple(self.agent_pos) == tuple(self.goal_pos):
+                reward = 1
+            else:
+                reward = 0
+            done = True
+        else:
+            dr, dc = moves[action]
+            new_row = self.agent_pos[0] + dr
+            new_col = self.agent_pos[1] + dc
+
+            # only move if valid open path -> maybe change to fail immediately later
+            if self.maze[new_row, new_col] == 0:
+                self.agent_pos = (new_row, new_col)
+        
+        if self.steps >= self.max_steps:
+            done = True
+        
+        state = (self.maze, self.agent_pos, self.goal_pos)
+        return state, reward, done
+
